124f1f3624a2325bc2d4304e2228d27971cc73d0
antoclus <antolustraducciones@gmail.com>
2023-10-20 14:38:14 +0200

    styling and functionality

--- client/greenfieldt3-app/src/components/signupForm.js
+++ client/greenfieldt3-app/src/components/signupForm.js
@@ -1,38 +1,68 @@
 import React, { Component } from 'react'
+import { useState } from 'react'
 import './loginSignup.css'
 
 export default class SignUp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            password: "",
+            name: "",
+            lastName: ""
+        }
+    }
+
+    handleInputChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+    handleRegister = (e) => {
+        e.preventDefault();
+        const { name, lastName, email, password } = this.state;
+        console.log("Form Data:", { name, lastName, email, password });
+        
+    }
     render() {
         return (
             <form>
                 <h3>Sign Up</h3>
 
-                <div className="mb-3">
-                    <label></label>
+                <div className="mb-3" onSubmit={this.handleRegister}>
                     <input
                         type="text"
+                        name="firstName"
                         className="form-control"
                         placeholder="First name"
+                        onChange={this.handleInputChange}
+                    
+                   
                     />
                 </div>
-                <div className="mb-3">
-                    <label></label>
-                    <input type="text" className="form-control" placeholder="Last name" />
+                <div className="mb-3" onSubmit={this.handleRegister}>
+                    <input type="text" 
+                    name = "lastName"
+                    className="form-control" 
+                    placeholder="Last name"
+                    onChange={this.handleInputChange}/>
                 </div>
-                <div className="mb-3">
-                    <label></label>
+                <div className="mb-3" onSubmit={this.handleRegister}>
                     <input
                         type="email"
+                        name = "email"
                         className="form-control"
                         placeholder="Enter email"
+                        onChange={this.handleInputChange}
                     />
                 </div>
-                <div className="mb-3">
-                    <label></label>
+                <div className="mb-3"onSubmit={this.handleRegister}>
                     <input
                         type="password"
+                        name ="password"
                         className="form-control"
                         placeholder="Enter password"
+                        onChange={this.handleInputChange}
                     />
                 </div>
                 <div className="d-grid">
@@ -47,3 +77,4 @@ export default class SignUp extends Component {
         )
     }
 }
+
